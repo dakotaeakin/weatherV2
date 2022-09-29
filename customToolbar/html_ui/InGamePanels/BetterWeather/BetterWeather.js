7740398b51ef61b15ab68fa004e2433fa7ce5ace
xhellcat1942x <75864133+xhellcat1942x@users.noreply.github.com>
2022-09-28 21:03:17 -0600

    Checking feasibility to add new toolbar
    Would like to add new toolbar instead of editing built in one.
    Found that built in toolbar is used to push the weather data to the sim.

---
+++ customToolbar/html_ui/InGamePanels/BetterWeather/BetterWeather.js
@@ -0,0 +1,20 @@
+class WeatherToolbarPanelElement extends UIElement {
+  connectedCallback() {
+    super.connectedCallback();
+  }
+}
+window.customElements.define(
+  "weather-toolbar-panel",
+  WeatherToolbarPanelElement
+);
+checkAutoload();
+if (EDITION_MODE()) {
+  document.documentElement.style.backgroundColor = "teal";
+  document.addEventListener("DOMContentLoaded", () => {
+    setTimeout(() => {
+      document.querySelector("ingame-ui").classList.remove("panelInvisible");
+      document.querySelector("ingame-ui").classList.add("attached");
+    }, 200);
+  });
+}
+//# sourceMappingURL=WeatherToolbarPanel.js.map
